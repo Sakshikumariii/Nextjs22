2d5f7190eaab54065557fdf0ed2634335736d492
Sakshi-Kumari07 <sakshi.k@testyantra.in>
2025-12-22 18:25:37 +0530

    Next js

--- src/app/isr-demo/page.js
+++ src/app/isr-demo/page.js
@@ -1,10 +1,22 @@
-
 export const revalidate = 10;
+async function getPost() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    next: { revalidate: 10 },
+  });
+
+  return res.json();
+}
 
 export default async function ISRPage() {
+  const post = await getPost();
+
   return (
     <main>
       <h1>ISR Demo</h1>
+
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+
       <p>Generated at:</p>
       <p>{new Date().toLocaleTimeString()}</p>
     </main>
